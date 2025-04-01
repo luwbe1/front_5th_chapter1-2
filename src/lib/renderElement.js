5d8cc7e350e25fb44a743b19ca8ccfb4e59e2417
luwbe1 <luwbe1@naver.com>
2025-04-01 21:40:46 +0900

    Feature: renderElement 구현

--- src/lib/renderElement.js
+++ src/lib/renderElement.js
@@ -7,4 +7,27 @@ export function renderElement(vNode, container) {
   // 최초 렌더링시에는 createElement로 DOM을 생성하고
   // 이후에는 updateElement로 기존 DOM을 업데이트한다.
   // 렌더링이 완료되면 container에 이벤트를 등록한다.
+
+  // vNode가 null이면 container의 innerHTML을 ""로 초기화한다.
+  if (!vNode) {
+    container.innerHTML = "";
+    return;
+  }
+  // vNode가 null이 아니면 vNode를 normalizeVNode로 정규화한다.
+  vNode = normalizeVNode(vNode);
+
+  // vNode가 DOM이 아니면 vNode를 createElement로 DOM으로 변환한다.
+  const oldNode = container.firstChild;
+  const newElement = createElement(vNode);
+
+  // vNode가 DOM이면 vNode를 createElement로 DOM으로 변환한다.
+  if (oldNode) {
+    updateElement(oldNode, oldNode.vNode, vNode);
+    container.replaceChild(newElement, oldNode);
+  } else {
+    container.appendChild(newElement);
+  }
+
+  // 이벤트 핸들러를 위임 방식으로 등록
+  setupEventListeners(container); // 부모 요소인 container에 이벤트를 위임
 }
