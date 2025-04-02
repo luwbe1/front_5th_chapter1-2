8033eb43577671d41d9d6d9efc58c3e57642f97c
luwbe1 <luwbe1@naver.com>
2025-04-02 23:40:29 +0900

    Chore: 메인 버전 / 해시 버전 추가

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig as defineTestConfig, mergeConfig } from "vitest/config";
 import { defineConfig } from "vite";
+import { resolve } from "path";
 
 export default mergeConfig(
   defineConfig({
@@ -10,6 +11,14 @@ export default mergeConfig(
       esbuildOptions: {
         jsx: "transform",
         jsxFactory: "createVNode",
+      },
+    },
+    build: {
+      rollupOptions: {
+        input: {
+          main: resolve(__dirname, "index.html"), // 메인 엔트리 파일
+          hash: resolve(__dirname, "index.hash.html"), // 해시 페이지 포함
+        },
       },
     },
     base: "/front-5th-chapter1-2/",
